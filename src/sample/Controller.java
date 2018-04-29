5fe8f900c43469883cf3705f9dfd3cce3f071754
Øystein <oysteinha@gmail.com>
2018-04-29 16:35:04 +0200

    Button generates a random number in the console.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.util.Random;
+
 public class Controller {
+
+    public void generateRandom(ActionEvent event) {
+        Random random = new Random();
+        int myRandomNumber = random.nextInt(50) + 1;
+        System.out.println(myRandomNumber);
+    }
+
 }
